592e089e954c5d3b6b908c4874a11de59cf5c480
nicolargo <nicolashennion@gmail.com>
2024-07-06 14:28:01 +0200

    Day 1 part 1 and 2 success

--- day01/day01-part2.py
+++ day01/day01-part2.py
@@ -2,24 +2,34 @@
 
 import argparse
 import logging
+import re
 import sys
 
 logger = logging.getLogger(__name__)
 
 TRANSFORMS = [
-    {"letters": "one", "digits": "1", "position": "-1"},
-    {"letters": "two", "digits": "2", "position": "-1"},
-    {"letters": "three", "digits": "3", "position": "-1"},
-    {"letters": "four", "digits": "4", "position": "-1"},
-    {"letters": "five", "digits": "5", "position": "-1"},
-    {"letters": "six", "digits": "6", "position": "-1"},
-    {"letters": "seven", "digits": "7", "position": "-1"},
-    {"letters": "eight", "digits": "8", "position": "-1"},
-    {"letters": "nine", "digits": "9", "position": "-1"},
+    {"letters": "1", "digits": "1"},
+    {"letters": "2", "digits": "2"},
+    {"letters": "3", "digits": "3"},
+    {"letters": "4", "digits": "4"},
+    {"letters": "5", "digits": "5"},
+    {"letters": "6", "digits": "6"},
+    {"letters": "7", "digits": "7"},
+    {"letters": "8", "digits": "8"},
+    {"letters": "9", "digits": "9"},
+    {"letters": "one", "digits": "1"},
+    {"letters": "two", "digits": "2"},
+    {"letters": "three", "digits": "3"},
+    {"letters": "four", "digits": "4"},
+    {"letters": "five", "digits": "5"},
+    {"letters": "six", "digits": "6"},
+    {"letters": "seven", "digits": "7"},
+    {"letters": "eight", "digits": "8"},
+    {"letters": "nine", "digits": "9"},
 ]
 
 parser = argparse.ArgumentParser(
-    prog="day02",
+    prog="day01-part2",
     conflict_handler="resolve",
     formatter_class=argparse.RawDescriptionHelpFormatter,
 )
@@ -32,7 +42,11 @@ parser.add_argument(
     help="enable debug mode",
 )
 parser.add_argument(
-    "-i", "--input", default="./puzzle_test.txt", dest="input_file", help="Input puzzle"
+    "-i",
+    "--input",
+    default="./puzzle_test-part2.txt",
+    dest="input_file",
+    help="Input puzzle",
 )
 args = parser.parse_args()
 if args.debug:
@@ -52,22 +66,28 @@ with open(args.input_file, "r") as f:
         logger.debug("*" * 80)
         logger.debug(line)
 
-        transforms_copy = TRANSFORMS.copy()
-        # Find letters position in the string
-        for i in transforms_copy:
-            i["position"] = line.find(i["letters"])
-        # Remove not found
-        transforms_copy = [x for x in transforms_copy if x["position"] != -1]
+        transforms_result = []
+        # Find all "letters" positions in the string
+        for i in TRANSFORMS:
+            for p in [m.start() for m in re.finditer(i["letters"], line)]:
+                transforms_result.append(
+                    {
+                        "letters": i["letters"],
+                        "digits": i["digits"],
+                        "position": p,
+                    }
+                )
+
         # Sort
-        transforms_copy.sort(key=lambda x: x["position"])
+        transforms_result.sort(key=lambda x: x["position"])
         # Only take into account first one and last one
-        logger.debug(transforms_copy)
-        if len(transforms_copy) > 0:
-            transforms_copy = [transforms_copy[0], transforms_copy[-1]]
+        logger.debug(transforms_result)
+        if len(transforms_result) > 0:
+            transforms_result = [transforms_result[0], transforms_result[-1]]
 
         # Replace letters by digits
-        logger.debug(transforms_copy)
-        for i in transforms_copy:
+        logger.debug(transforms_result)
+        for i in transforms_result:
             line = line.replace(i["letters"], i["digits"])
         logger.debug(line)
 
